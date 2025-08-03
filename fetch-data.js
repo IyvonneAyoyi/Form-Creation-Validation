e937333eaf93c3db62c53997caf28e952ca462dd
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-03 14:35:57 +0300

    Select data container element

--- fetch-data.js
+++ fetch-data.js
@@ -2,3 +2,11 @@
 async function fetchUserData() {
     // Define the API URL variable
   const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  // Select data container element
+  const dataContainer = document.getElementById('api-data');
+  if (!dataContainer) {
+    console.error('Element with ID "api-data" not found.');
+    return;
+  }
+
